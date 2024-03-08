efe7fe9f1311ea55d65fcc6766408ff8307832ce
NicolaiTue <nicolai.tue@gmail.com>
2024-03-08 23:50:29 +0100

    enemy

---
+++ Assets/Scripts/Fjender/AiEnemy.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiEnemy : MonoBehaviour
+{
+
+    public GameObject Player;
+    public float speed;
+
+    public string tæt_på_player;
+    public float detectionRadius = 5f;
+    private bool playerInRange = false;
+    Animator anim;
+
+
+    private float distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector2.Distance(transform.position, Player.transform.position);
+        Vector2 direction = Player.transform.forward - transform.forward;
+
+        transform.position = Vector2.MoveTowards(this.transform.position, Player.transform.position, speed * Time.deltaTime);
+
+
+        // kører funktionerne som gør at den chekker om den attacke
+        CheckForPlayer();
+        UpdateAnimation();
+    }
+
+    void CheckForPlayer()
+    {
+        // Assume the player has a "Player" tag. Adjust as needed.
+        GameObject Player = GameObject.FindGameObjectWithTag("Player");
+
+        if (Player != null)
+        {
+            float distanceToPlayer = Vector2.Distance(transform.position, Player.transform.position);
+
+            if (distanceToPlayer <= detectionRadius)
+            {
+                playerInRange = true;
+            }
+            else
+            {
+                playerInRange = false;
+            }
+        }
+    }
+
+
+    void UpdateAnimation()
+    {
+        
+        if (playerInRange)
+        {
+            anim.SetTrigger(tæt_på_player);
+        }
+    }
+
+}
