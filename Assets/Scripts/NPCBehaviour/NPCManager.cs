3b2face3caf179577b6140141f2f786add697aa0
Sebastian Tirsgaard Larsen <sebtirsgaard@gmail.com>
2024-04-17 12:38:59 +0200

    NPCManager

---
+++ Assets/Scripts/NPCBehaviour/NPCManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class NPCManager : MonoBehaviour
+{
+    [Header("Components")]
+    public TextMeshProUGUI NameSpace;
+
+    [Header("NPC Names")]
+    public string NameHolder = "NameHolder";
+
+    public string[] Names = new string[] { };
+
+}
