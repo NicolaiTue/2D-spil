5ae90e29d50d118bbb163421e57a9806f683e6ee
Sebastian Tirsgaard Larsen <sebtirsgaard@gmail.com>
2024-04-17 13:27:18 +0200

    Finished NPCInteractions

--- Assets/Scripts/NPCBehaviour/NPCManager.cs
+++ Assets/Scripts/NPCBehaviour/NPCManager.cs
@@ -2,15 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NPCManager : MonoBehaviour
 {
     [Header("Components")]
     public TextMeshProUGUI NameSpace;
+    public RawImage WhoIsTalkingPic;
 
     [Header("NPC Names")]
     public string NameHolder = "NameHolder";
 
     public string[] Names = new string[] { };
+    [Header("NPC Pics")]
+    public Texture2D[] texures = new Texture2D[] { };
+    public Texture2D textureHolder;
+    private void Start()
+    {
+        
+    }
 
+    private void Update()
+    {
+        
+        NameSpace.text = NameHolder;
+        WhoIsTalkingPic.texture = textureHolder;
+
+    }
 }
