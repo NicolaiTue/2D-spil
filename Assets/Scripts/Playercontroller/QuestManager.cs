cc5e327bf54ba0a30b8ee659baa357d74f71b5b6
Sebastian Tirsgaard Larsen <sebtirsgaard@gmail.com>
2024-04-25 08:51:13 +0200

    Player update

---
+++ Assets/Scripts/Playercontroller/QuestManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    public bool isQuestDone = false;
+    public bool isQuestFailed = false;
+    public GameObject[] QuestScripts = new GameObject[] { };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
