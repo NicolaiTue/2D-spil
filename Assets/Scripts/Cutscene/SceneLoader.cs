f1ae7627591ab92d6fff3c7454da31160c36361b
StivDolkUnderBord <ninjakai284@gmail.com>
2024-04-30 14:51:51 +0200

    cutscene HJÆLP PLZ

--- Assets/Scripts/Cutscene/SceneLoader.cs
+++ Assets/Scripts/Cutscene/SceneLoader.cs
@@ -4,8 +4,30 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class SceneLoader : MonoBehaviour
 {
+    public float timeDelay;
+    float newTime;
+    bool startTimer = false;    
+
+
+
+    void start()
+    {
+        CutsceneT();
+    }
+
     public void LoadScene()
     {
-        SceneManager.LoadScene("Tutorial");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    void CutsceneT()
+    {
+        newTime = Time.time + timeDelay;
+        startTimer = true;
+        if (Time.time > newTime && startTimer)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
 }
