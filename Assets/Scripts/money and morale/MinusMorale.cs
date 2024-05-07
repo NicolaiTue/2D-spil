6f59eb72c841f642adbf0f7dc6614091682b8f75
NicolaiTue <nicolai.tue@gmail.com>
2024-05-07 08:40:28 +0200

    s

--- Assets/Scripts/money and morale/MinusMorale.cs
+++ Assets/Scripts/money and morale/MinusMorale.cs
@@ -2,20 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AddMoraleScript : MonoBehaviour
+public class MinusMorale : MonoBehaviour
 {
 
     public int Moraleamount;
     private bool addedMorale = false; //  for at sikre, at morale kun tilføjes én gang
 
-    private void OnEnable()
+    private void OnDestroy()
     {
         if (!addedMorale)
         {
             // Tilføj 20 moral, når objektet aktiveres første gang
-            GameManager.instance.AddMorale(Moraleamount);
+            GameManager.instance.SubtractMorale(Moraleamount);
             addedMorale = true; // Sæt addedMorale til sandt for at forhindre gentagen tilføjelse af morale
-            Debug.Log(Moraleamount);
+            Debug.Log("-"+Moraleamount);
         }
     }
 }
